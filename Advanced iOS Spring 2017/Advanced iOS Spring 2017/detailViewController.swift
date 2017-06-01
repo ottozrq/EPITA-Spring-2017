bde30d0f1ab34f6d1b1a1622699c70f2d2f26d2b
Ruoqiu Zhang <zhag_r@epita.fr>
2017-06-01 21:06:04 +0200

    [6th class] being able to add info into the tableview (adding some missing files)

---
+++ Advanced iOS Spring 2017/Advanced iOS Spring 2017/detailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  detailViewController.swift
+//  Advanced iOS Spring 2017
+//
+//  Created by 张若秋 on 2017/6/1.
+//  Copyright © 2017年 ___AdvancediOS___. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    @IBOutlet weak var tmpLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Advanced iOS Spring 2017/Advanced iOS Spring 2017/newViewController.swift
@@ -0,0 +1,59 @@
+//
+//  newViewController.swift
+//  Advanced iOS Spring 2017
+//
+//  Created by 张若秋 on 2017/6/1.
+//  Copyright © 2017年 ___AdvancediOS___. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class newViewController: UIViewController {
+    
+    var weather: Weather?
+    
+    @IBOutlet weak var cityText: UITextField!
+    
+    @IBOutlet weak var tempText: UITextField!
+    
+    @IBAction func cancelBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var saveBtn: UIBarButtonItem!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard let button = sender as? UIBarButtonItem, button === saveBtn else {
+            os_log("fail", log: OSLog.default, type: .debug)
+            return
+        }
+        let city = cityText.text
+        let temp = Int(tempText.text!)
+        weather = Weather(city: city!, temp: temp!, pic: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
